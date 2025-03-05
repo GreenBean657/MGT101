649c736dc1c46b66aabbc3656eb260ec681d74ca
Robert Goodman <rgoodman22506@gmail.com>
2025-03-05 10:52:40 -0700

    Added view all goals structure

--- src/main/java/org/BankingSystem/controllers/MainController.java
+++ src/main/java/org/BankingSystem/controllers/MainController.java
@@ -6,13 +6,7 @@ import org.BankingSystem.views.UserLoginView;
 public class MainController {
 
     public void run() {
-
-
         SQLController.loadDB();
-        SQLController.write(new Human(1, "Bob", 9, "1235", new String[]{}, new String[]{}));
-        Human human = SQLController.load(1);
-        assert human != null;
-        System.out.println(human.toString());
         UserLoginView.loginScreen();
     }
 }
--- src/main/java/org/BankingSystem/views/UserGoalView.java
+++ src/main/java/org/BankingSystem/views/UserGoalView.java
@@ -1,5 +1,6 @@
 package org.BankingSystem.views;
 
+import org.BankingSystem.controllers.SQLController;
 import org.BankingSystem.models.Human;
 
 import static org.BankingSystem.views.UserLoginView.scanner;
@@ -19,7 +20,29 @@ public class UserGoalView {
         loadedhuman.setGoals(newGoals);
 
         for (String loadedGoal : newGoals) {
-            System.out.print(loadedGoal + " ");
+            System.out.print(loadedGoal + " \n");
         }
+
+        System.out.println("Would like to view all your goals \n" + "1. Yes\n" + "2. No \n");
+       do {
+           String answer = scanner.nextLine();
+           if (answer.equals("1")) {
+               userGoalView();
+           } else if (answer.equals("2")) {
+               System.out.println("opening menuView again\n");
+           } else {
+               System.out.println("Invalid answer, please try again\n");
+           }
+       }while (true);
+    }
+
+    public static void userGoalView(){
+
+
+        Human[] humans = SQLController.loadAll();
+        for (Human human : humans) {
+            System.out.print(human.getGoals()[0] + " ");
+        }
+
     }
 }
